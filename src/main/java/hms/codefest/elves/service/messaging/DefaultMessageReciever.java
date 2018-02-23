17389f626f7f32b086fdf0c6f9789dfd0fdebe2e
gayanb <gayanb@hsenidmobile.com>
2018-02-23 16:51:34 +0530

    fixed test issues

--- src/main/java/hms/codefest/elves/service/messaging/DefaultMessageReciever.java
+++ src/main/java/hms/codefest/elves/service/messaging/DefaultMessageReciever.java
@@ -21,11 +21,11 @@ public class DefaultMessageReciever implements MessageReceiver{
 
         }else  if (msg.matches("^(100|[0-9]{2})$")) {
             replySender.pushMessage("Thank you for the update, would you like to save the change and publish the task?", userName);
+        } else if (msg.toLowerCase().equals("yes")) {
+            replySender.pushMessage("Thank you. We will notify the project manager regarding your update. Have a nice day!", userName);
             BasicTask submittedTaskByUser = taskProcessingService.findSubmittedTaskByUser(userName);
             taskProcessingService.submitTaskForUpdate(submittedTaskByUser, Integer.parseInt(msg));
             taskProcessingService.clearSubmittedTask(userName);
-        } else if (msg.toLowerCase().equals("yes")) {
-            replySender.pushMessage("Thank you. We will notify the project manager regarding your update. Have a nice day!", userName);
         } else {
             replySender.pushMessage("Thank you for the response, reply with 'notify' if you would like to get project managers attention " +
                     "on this. If not we will save your message in our action history, have a nice day.", userName);
